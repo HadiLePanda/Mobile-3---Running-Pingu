890e35f175422c4f9855a619cc4e575de0bc2635
Hadi <hadizorkot@gmail.com>
2024-10-16 17:34:18 +0000

    Added segments

--- Assets/Running Pingu/Scripts/LevelManager.cs
+++ Assets/Running Pingu/Scripts/LevelManager.cs
@@ -1,18 +1,138 @@
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class LevelManager : MonoBehaviour
 {
     [Header("References")]
-    public List<Obstacle> ramps = new();
-    public List<Obstacle> longBlocks = new();
-    public List<Obstacle> jumps = new();
-    public List<Obstacle> slides = new();
+    [SerializeField] private List<Obstacle> ramps = new();
+    [SerializeField] private List<Obstacle> longBlocks = new();
+    [SerializeField] private List<Obstacle> jumps = new();
+    [SerializeField] private List<Obstacle> slides = new();
+    [SerializeField] private List<Segment> availableSegments = new();
+    [SerializeField] private List<Segment> availableTransitions = new();
+    
+    [Header("Settings")]
+    [SerializeField] private float distanceBeforeSpawn = 100f;
+    [SerializeField] private int initialSegments = 10;
+    [SerializeField] private int maxSegmentsOnScreen = 15;
+    [SerializeField] private bool showCollider = false;
+
+    private Transform cameraContainer;
+    private int amountOfActiveSegments;
+    private int continuousSegments;
+    private int currentSpawnZ;
+    private int currentLevel;
+    private int y1, y2, y3;
 
     private List<Obstacle> spawnedObstacles = new(); // all the obstacles in the pool
+    private List<Segment> spawnedSegments = new();
 
     public static LevelManager Instance;
-    
+
+    private void Awake()
+    {
+        Instance = this;
+
+        cameraContainer = Camera.main.transform;
+        currentSpawnZ = 0;
+        currentLevel = 0;
+    }
+
+    private void Start()
+    {
+        GenerateInitialSegments();
+    }
+
+    private void GenerateInitialSegments()
+    {
+        for (int i = 0; i < initialSegments; i++)
+        {
+            GenerateSegment();
+        }
+    }
+
+    private void GenerateSegment()
+    {
+        SpawnSegment();
+
+        if (Random.Range(0f, 1f) < (continuousSegments * 0.25f))
+        {
+            // spawn transition segment
+            continuousSegments = 0;
+            SpawnTransition();
+        }
+        else
+        {
+            continuousSegments++;
+        }
+
+    }
+
+    private void SpawnSegment()
+    {
+        var possibleSegments = availableSegments.FindAll(x => x.beginY1 == y1 || x.beginY2 == y2 || x.beginY3 == y3);
+        int id = Random.Range(0, possibleSegments.Count);
+
+        Segment segment = GetSegment(id, false);
+
+        y1 = segment.endY1;
+        y2 = segment.endY2;
+        y3 = segment.endY3;
+
+        segment.transform.SetParent(transform);
+        segment.transform.localPosition = Vector3.forward * currentSpawnZ;
+
+        currentSpawnZ += segment.length;
+        amountOfActiveSegments++;
+        segment.Spawn();
+    }
+
+    private void SpawnTransition()
+    {
+        var possibleTransitions = availableTransitions.FindAll(x => x.beginY1 == y1 || x.beginY2 == y2 || x.beginY3 == y3);
+        int id = Random.Range(0, possibleTransitions.Count);
+
+        Segment segment = GetSegment(id, true);
+
+        y1 = segment.endY1;
+        y2 = segment.endY2;
+        y3 = segment.endY3;
+
+        segment.transform.SetParent(transform);
+        segment.transform.localPosition = Vector3.forward * currentSpawnZ;
+
+        currentSpawnZ += segment.length;
+        amountOfActiveSegments++;
+        segment.Spawn();
+    }
+
+    public Segment GetSegment(int id, bool isTransition)
+    {
+        Segment segment = null;
+        segment = spawnedSegments.Find(x => x.Id == id && x.isTransition == isTransition && !x.gameObject.activeSelf);
+
+        // not found, spawn a new segment
+        if (segment == null)
+        {
+            GameObject go = Instantiate(isTransition ? availableTransitions[id].gameObject : availableSegments[id].gameObject);
+            segment = go.GetComponent<Segment>();
+
+            segment.Id = id;
+            segment.isTransition = isTransition;
+
+            spawnedSegments.Insert(0, segment);
+        }
+        // segment found, reorder it
+        else
+        {
+            spawnedSegments.Remove(segment);
+            spawnedSegments.Insert(0, segment);
+        }
+
+        return segment;
+    }
+
     public Obstacle GetObstacle(ObstacleType type, int visualIndex)
     {
         // get an obstacle that is of:
